c1c8f5715990fa8a681efc5592542d1bf717f8e3
Bernie <ptyc4076@gmail.com>
2024-06-24 23:15:16 +0800

    Added some more components. 06/24

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -31,6 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.G_ModeNumeric = new System.Windows.Forms.Panel();
+            this.G1_AllowRegex = new System.Windows.Forms.CheckBox();
+            this.G_NumericL6 = new System.Windows.Forms.Label();
+            this.G_NumericMultiple = new System.Windows.Forms.TextBox();
+            this.G1_AllowMutiple = new System.Windows.Forms.CheckBox();
+            this.G_NumericL5 = new System.Windows.Forms.Label();
+            this.G_NumericDP = new System.Windows.Forms.TextBox();
+            this.G_NumericL4 = new System.Windows.Forms.Label();
+            this.G1_AllowDecimal = new System.Windows.Forms.CheckBox();
+            this.G_NumericL3 = new System.Windows.Forms.Label();
+            this.G_NumericL2 = new System.Windows.Forms.Label();
+            this.G_NumericRMax = new System.Windows.Forms.TextBox();
+            this.G_NumericRMin = new System.Windows.Forms.TextBox();
+            this.G_NumericL1 = new System.Windows.Forms.Label();
+            this.G_NumericTitle = new System.Windows.Forms.Label();
             this.Generation = new System.Windows.Forms.Panel();
             this.Suffix = new System.Windows.Forms.Label();
             this.Choices = new System.Windows.Forms.ComboBox();
@@ -44,21 +58,128 @@
             this.FileAccess = new System.Windows.Forms.Panel();
             this.O_ModeExport = new System.Windows.Forms.Panel();
             this.Signature = new System.Windows.Forms.Panel();
+            this.BernieName = new System.Windows.Forms.Label();
             this.Bernie = new System.Windows.Forms.Button();
             this.Title = new System.Windows.Forms.Panel();
             this.MenuBar = new System.Windows.Forms.MenuStrip();
             this.Description = new System.Windows.Forms.ToolTip(this.components);
             this.G_ModeName = new System.Windows.Forms.Panel();
-            this.BernieName = new System.Windows.Forms.Label();
+            this.G_NumericRegex = new System.Windows.Forms.TextBox();
+            this.G_NumericL7 = new System.Windows.Forms.Label();
+            this.O_ExportTitle = new System.Windows.Forms.Label();
+            this.O_ExportL1 = new System.Windows.Forms.Label();
+            this.O_ExportBatch = new System.Windows.Forms.TextBox();
+            this.O_ExportL2 = new System.Windows.Forms.Label();
+            this.O_ExportBatchSize = new System.Windows.Forms.TextBox();
+            this.O1_Unique = new System.Windows.Forms.CheckBox();
+            this.O_ExportL3 = new System.Windows.Forms.Label();
+            this.O1_SecondRandom = new System.Windows.Forms.CheckBox();
+            this.O1_Tabular = new System.Windows.Forms.CheckBox();
+            this.O1_Generate = new System.Windows.Forms.Button();
+            this.紀錄視窗ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.使用說明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存當前設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.G_ModeNumeric.SuspendLayout();
             this.Generation.SuspendLayout();
             this.Output.SuspendLayout();
+            this.O_ModeExport.SuspendLayout();
             this.Signature.SuspendLayout();
+            this.MenuBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // G_ModeNumeric
             // 
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL7);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericRegex);
+            this.G_ModeNumeric.Controls.Add(this.G1_AllowRegex);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL6);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericMultiple);
+            this.G_ModeNumeric.Controls.Add(this.G1_AllowMutiple);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL5);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericDP);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL4);
+            this.G_ModeNumeric.Controls.Add(this.G1_AllowDecimal);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL3);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL2);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericRMax);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericRMin);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericL1);
+            this.G_ModeNumeric.Controls.Add(this.G_NumericTitle);
             resources.ApplyResources(this.G_ModeNumeric, "G_ModeNumeric");
             this.G_ModeNumeric.Name = "G_ModeNumeric";
+            // 
+            // G1_AllowRegex
+            // 
+            resources.ApplyResources(this.G1_AllowRegex, "G1_AllowRegex");
+            this.G1_AllowRegex.Name = "G1_AllowRegex";
+            this.G1_AllowRegex.UseVisualStyleBackColor = true;
+            // 
+            // G_NumericL6
+            // 
+            resources.ApplyResources(this.G_NumericL6, "G_NumericL6");
+            this.G_NumericL6.Name = "G_NumericL6";
+            // 
+            // G_NumericMultiple
+            // 
+            resources.ApplyResources(this.G_NumericMultiple, "G_NumericMultiple");
+            this.G_NumericMultiple.Name = "G_NumericMultiple";
+            // 
+            // G1_AllowMutiple
+            // 
+            resources.ApplyResources(this.G1_AllowMutiple, "G1_AllowMutiple");
+            this.G1_AllowMutiple.Name = "G1_AllowMutiple";
+            this.G1_AllowMutiple.UseVisualStyleBackColor = true;
+            // 
+            // G_NumericL5
+            // 
+            resources.ApplyResources(this.G_NumericL5, "G_NumericL5");
+            this.G_NumericL5.Name = "G_NumericL5";
+            // 
+            // G_NumericDP
+            // 
+            resources.ApplyResources(this.G_NumericDP, "G_NumericDP");
+            this.G_NumericDP.Name = "G_NumericDP";
+            // 
+            // G_NumericL4
+            // 
+            resources.ApplyResources(this.G_NumericL4, "G_NumericL4");
+            this.G_NumericL4.Name = "G_NumericL4";
+            // 
+            // G1_AllowDecimal
+            // 
+            resources.ApplyResources(this.G1_AllowDecimal, "G1_AllowDecimal");
+            this.G1_AllowDecimal.Name = "G1_AllowDecimal";
+            this.G1_AllowDecimal.UseVisualStyleBackColor = true;
+            // 
+            // G_NumericL3
+            // 
+            resources.ApplyResources(this.G_NumericL3, "G_NumericL3");
+            this.G_NumericL3.Name = "G_NumericL3";
+            // 
+            // G_NumericL2
+            // 
+            resources.ApplyResources(this.G_NumericL2, "G_NumericL2");
+            this.G_NumericL2.Name = "G_NumericL2";
+            // 
+            // G_NumericRMax
+            // 
+            resources.ApplyResources(this.G_NumericRMax, "G_NumericRMax");
+            this.G_NumericRMax.Name = "G_NumericRMax";
+            // 
+            // G_NumericRMin
+            // 
+            resources.ApplyResources(this.G_NumericRMin, "G_NumericRMin");
+            this.G_NumericRMin.Name = "G_NumericRMin";
+            // 
+            // G_NumericL1
+            // 
+            resources.ApplyResources(this.G_NumericL1, "G_NumericL1");
+            this.G_NumericL1.Name = "G_NumericL1";
+            // 
+            // G_NumericTitle
+            // 
+            resources.ApplyResources(this.G_NumericTitle, "G_NumericTitle");
+            this.G_NumericTitle.Name = "G_NumericTitle";
             // 
             // Generation
             // 
@@ -142,6 +263,16 @@
             // 
             // O_ModeExport
             // 
+            this.O_ModeExport.Controls.Add(this.O1_Generate);
+            this.O_ModeExport.Controls.Add(this.O1_Tabular);
+            this.O_ModeExport.Controls.Add(this.O1_SecondRandom);
+            this.O_ModeExport.Controls.Add(this.O_ExportL3);
+            this.O_ModeExport.Controls.Add(this.O1_Unique);
+            this.O_ModeExport.Controls.Add(this.O_ExportBatchSize);
+            this.O_ModeExport.Controls.Add(this.O_ExportL2);
+            this.O_ModeExport.Controls.Add(this.O_ExportBatch);
+            this.O_ModeExport.Controls.Add(this.O_ExportL1);
+            this.O_ModeExport.Controls.Add(this.O_ExportTitle);
             resources.ApplyResources(this.O_ModeExport, "O_ModeExport");
             this.O_ModeExport.Name = "O_ModeExport";
             // 
@@ -153,6 +284,12 @@
             this.Signature.ForeColor = System.Drawing.SystemColors.ControlText;
             resources.ApplyResources(this.Signature, "Signature");
             this.Signature.Name = "Signature";
+            // 
+            // BernieName
+            // 
+            resources.ApplyResources(this.BernieName, "BernieName");
+            this.BernieName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BernieName.Name = "BernieName";
             // 
             // Bernie
             // 
@@ -170,6 +307,10 @@
             // 
             // MenuBar
             // 
+            this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.儲存當前設定ToolStripMenuItem,
+            this.紀錄視窗ToolStripMenuItem,
+            this.使用說明ToolStripMenuItem});
             resources.ApplyResources(this.MenuBar, "MenuBar");
             this.MenuBar.Name = "MenuBar";
             // 
@@ -178,11 +319,88 @@
             resources.ApplyResources(this.G_ModeName, "G_ModeName");
             this.G_ModeName.Name = "G_ModeName";
             // 
-            // BernieName
+            // G_NumericRegex
             // 
-            resources.ApplyResources(this.BernieName, "BernieName");
-            this.BernieName.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.BernieName.Name = "BernieName";
+            resources.ApplyResources(this.G_NumericRegex, "G_NumericRegex");
+            this.G_NumericRegex.Name = "G_NumericRegex";
+            // 
+            // G_NumericL7
+            // 
+            resources.ApplyResources(this.G_NumericL7, "G_NumericL7");
+            this.G_NumericL7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.G_NumericL7.Name = "G_NumericL7";
+            // 
+            // O_ExportTitle
+            // 
+            resources.ApplyResources(this.O_ExportTitle, "O_ExportTitle");
+            this.O_ExportTitle.Name = "O_ExportTitle";
+            // 
+            // O_ExportL1
+            // 
+            resources.ApplyResources(this.O_ExportL1, "O_ExportL1");
+            this.O_ExportL1.Name = "O_ExportL1";
+            // 
+            // O_ExportBatch
+            // 
+            resources.ApplyResources(this.O_ExportBatch, "O_ExportBatch");
+            this.O_ExportBatch.Name = "O_ExportBatch";
+            // 
+            // O_ExportL2
+            // 
+            resources.ApplyResources(this.O_ExportL2, "O_ExportL2");
+            this.O_ExportL2.Name = "O_ExportL2";
+            // 
+            // O_ExportBatchSize
+            // 
+            resources.ApplyResources(this.O_ExportBatchSize, "O_ExportBatchSize");
+            this.O_ExportBatchSize.Name = "O_ExportBatchSize";
+            // 
+            // O1_Unique
+            // 
+            resources.ApplyResources(this.O1_Unique, "O1_Unique");
+            this.O1_Unique.Name = "O1_Unique";
+            this.Description.SetToolTip(this.O1_Unique, resources.GetString("O1_Unique.ToolTip"));
+            this.O1_Unique.UseVisualStyleBackColor = true;
+            // 
+            // O_ExportL3
+            // 
+            resources.ApplyResources(this.O_ExportL3, "O_ExportL3");
+            this.O_ExportL3.Name = "O_ExportL3";
+            // 
+            // O1_SecondRandom
+            // 
+            resources.ApplyResources(this.O1_SecondRandom, "O1_SecondRandom");
+            this.O1_SecondRandom.Name = "O1_SecondRandom";
+            this.Description.SetToolTip(this.O1_SecondRandom, resources.GetString("O1_SecondRandom.ToolTip"));
+            this.O1_SecondRandom.UseVisualStyleBackColor = true;
+            // 
+            // O1_Tabular
+            // 
+            resources.ApplyResources(this.O1_Tabular, "O1_Tabular");
+            this.O1_Tabular.Name = "O1_Tabular";
+            this.Description.SetToolTip(this.O1_Tabular, resources.GetString("O1_Tabular.ToolTip"));
+            this.O1_Tabular.UseVisualStyleBackColor = true;
+            // 
+            // O1_Generate
+            // 
+            resources.ApplyResources(this.O1_Generate, "O1_Generate");
+            this.O1_Generate.Name = "O1_Generate";
+            this.O1_Generate.UseVisualStyleBackColor = true;
+            // 
+            // 紀錄視窗ToolStripMenuItem
+            // 
+            this.紀錄視窗ToolStripMenuItem.Name = "紀錄視窗ToolStripMenuItem";
+            resources.ApplyResources(this.紀錄視窗ToolStripMenuItem, "紀錄視窗ToolStripMenuItem");
+            // 
+            // 使用說明ToolStripMenuItem
+            // 
+            this.使用說明ToolStripMenuItem.Name = "使用說明ToolStripMenuItem";
+            resources.ApplyResources(this.使用說明ToolStripMenuItem, "使用說明ToolStripMenuItem");
+            // 
+            // 儲存當前設定ToolStripMenuItem
+            // 
+            this.儲存當前設定ToolStripMenuItem.Name = "儲存當前設定ToolStripMenuItem";
+            resources.ApplyResources(this.儲存當前設定ToolStripMenuItem, "儲存當前設定ToolStripMenuItem");
             // 
             // MainForm
             // 
@@ -201,12 +419,18 @@
             this.MainMenuStrip = this.MenuBar;
             this.Name = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.G_ModeNumeric.ResumeLayout(false);
+            this.G_ModeNumeric.PerformLayout();
             this.Generation.ResumeLayout(false);
             this.Generation.PerformLayout();
             this.Output.ResumeLayout(false);
             this.Output.PerformLayout();
+            this.O_ModeExport.ResumeLayout(false);
+            this.O_ModeExport.PerformLayout();
             this.Signature.ResumeLayout(false);
             this.Signature.PerformLayout();
+            this.MenuBar.ResumeLayout(false);
+            this.MenuBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,6 +458,35 @@
         private System.Windows.Forms.Panel G_ModeName;
         private System.Windows.Forms.Button Bernie;
         private System.Windows.Forms.Label BernieName;
+        private System.Windows.Forms.Label G_NumericL1;
+        private System.Windows.Forms.Label G_NumericTitle;
+        private System.Windows.Forms.Label G_NumericL3;
+        private System.Windows.Forms.Label G_NumericL2;
+        private System.Windows.Forms.TextBox G_NumericRMax;
+        private System.Windows.Forms.TextBox G_NumericRMin;
+        private System.Windows.Forms.CheckBox G1_AllowDecimal;
+        private System.Windows.Forms.Label G_NumericL5;
+        private System.Windows.Forms.TextBox G_NumericDP;
+        private System.Windows.Forms.Label G_NumericL4;
+        private System.Windows.Forms.CheckBox G1_AllowMutiple;
+        private System.Windows.Forms.Label G_NumericL6;
+        private System.Windows.Forms.TextBox G_NumericMultiple;
+        private System.Windows.Forms.CheckBox G1_AllowRegex;
+        private System.Windows.Forms.Label G_NumericL7;
+        private System.Windows.Forms.TextBox G_NumericRegex;
+        private System.Windows.Forms.Label O_ExportTitle;
+        private System.Windows.Forms.Button O1_Generate;
+        private System.Windows.Forms.CheckBox O1_Tabular;
+        private System.Windows.Forms.CheckBox O1_SecondRandom;
+        private System.Windows.Forms.Label O_ExportL3;
+        private System.Windows.Forms.CheckBox O1_Unique;
+        private System.Windows.Forms.TextBox O_ExportBatchSize;
+        private System.Windows.Forms.Label O_ExportL2;
+        private System.Windows.Forms.TextBox O_ExportBatch;
+        private System.Windows.Forms.Label O_ExportL1;
+        private System.Windows.Forms.ToolStripMenuItem 儲存當前設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 紀錄視窗ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 使用說明ToolStripMenuItem;
     }
 }
 
